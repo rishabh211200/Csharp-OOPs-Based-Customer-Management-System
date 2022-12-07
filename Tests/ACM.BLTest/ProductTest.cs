1a62f2e7fd74ee83a2153454aa6e2660dfaddb34
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-07 13:09:36 +0530

    Added testing for prduct class in Business Logic

---
+++ Tests/ACM.BLTest/ProductTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomerManagementSystem.BL;
+
+namespace CMS.BLTest
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void ValidateProductName()
+        {
+            
+
+        }
+    }
+}
