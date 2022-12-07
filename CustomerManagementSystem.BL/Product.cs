8ac6657315beae2295641a9f1bdb2feb71f085a7
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-07 10:24:25 +0530

    Populated Product Class

--- CustomerManagementSystem.BL/Product.cs
+++ CustomerManagementSystem.BL/Product.cs
@@ -8,5 +8,51 @@ namespace CustomerManagementSystem.BL
 {
     public class Product
     {
+        public Product()
+        {
+
+        }
+        public Product(int productID)
+        {
+            ProductID = productID;
+        }
+        public decimal? CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductId { get; private set; }
+        public string ProductName { get; set; }
+
+        ///<summary>
+        /// Retrieve one product.
+        ///</summary>
+        public Product Retrieve(int productId)
+        {
+            // Code that retrieve the defined product
+            return new Product();
+        }
+
+        ///<summary>
+        /// Saves the current product
+        ///</summary>
+        ///<returns></returns>
+        public bool Save()
+        {
+            //Code that saves the defined product
+
+            return true;
+        }
+
+        ///<summary>
+        /// Validates the Product data.
+        ///</summary>
+        ///<returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+        }
     }
 }
