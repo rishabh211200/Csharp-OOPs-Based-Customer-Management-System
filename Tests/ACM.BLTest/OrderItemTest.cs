bef1025e748f876880884d00a3ee3a4565bfcfdf
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-07 13:46:25 +0530

    Written test cases for order validation

---
+++ Tests/ACM.BLTest/OrderItemTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomerManagementSystem.BL;
+
+namespace CMS.BLTest
+{
+    [TestClass]
+    public class OrderItemTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tests/ACM.BLTest/OrderTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomerManagementSystem.BL;
+
+namespace CMS.BLTest
+{
+    [TestClass]
+    public class OrderTest
+    {
+        [TestMethod]
+        public void ValidateValidOrderDate()
+        {
+            //--Arrange
+            Order order = new Order()
+            {
+                OrderDate = DateTimeOffset.MaxValue
+            };
+            var expected = true;
+            //--Act
+            var actual = order.Validate();
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ValidateNullOrderdate()
+        {
+            //--Arrange
+            Order order = new Order()
+            {
+                OrderDate = null
+            };
+            var expected = false;
+            //--Act
+            var actual = order.Validate();
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
