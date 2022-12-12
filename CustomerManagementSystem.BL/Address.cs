8899b388adb14189305a64f07e5b48b1703fff01
Rishabh Gupta <rishabh211200@gmail.com>
2022-12-12 11:52:57 +0530

    Added Address Class and AddressTest

---
+++ CustomerManagementSystem.BL/Address.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagementSystem.BL
+{
+    /**************************In order to reduce coupling we seperated address********************************/
+
+    public class Address
+    {
+        public Address()
+        {
+
+        }
+        public Address(int addressId)
+        {
+            AddressId = addressId;
+        }
+        public string StreetLine1 { get; set; }
+        public string StreetLine2 { get; set; }
+        public int AddressId { get; private set; }
+        public int AddressType { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set; }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (PostalCode == null) isValid = false;
+            return isValid;
+        }
+    
+    }
+}
---
+++ Tests/ACM.BLTest/AddressTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomerManagementSystem.BL;
+
+namespace CMS.BLTest
+{
+    [TestClass]
+    public class AddressTest
+    {
+        [TestMethod]
+        public void ValidPostalCode()
+        {
+            //--Arrange
+            Address address = new Address()
+            {
+                PostalCode = "110053"
+            };
+            var expected = true;
+            //--Act
+
+            var actual = address.Validate();
+            //--Assert
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NullPostalCode()
+        {
+            //--Arrange
+            Address address = new Address()
+            {
+                PostalCode = null
+            };
+            var expected = false;
+            //--Act
+
+            var actual = address.Validate();
+            //--Assert
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
